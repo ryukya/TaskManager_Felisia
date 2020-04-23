6d31f40dc66fc4686057b80a445e983170e49c0f
ryukya <felisiaryukiya@gmail.com>
2020-04-23 10:36:31 +0200

    change type on tasks

--- app/src/main/java/com/example/taskmanager/NoteActivity.kt
+++ app/src/main/java/com/example/taskmanager/NoteActivity.kt
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
 import android.widget.Toast
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_note.*
 
 class NoteActivity : AppCompatActivity() {
     var id = 0
     var types= "Meeting"
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +42,24 @@ class NoteActivity : AppCompatActivity() {
             }
         } catch (ex: Exception) {
         }
+       /* spinner = findViewById(R.id.sType)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.type_arrays,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val text: String = parent?.getItemAtPosition(position).toString()
+        types = text
+    } */
+
 
         btSave.setOnClickListener {
             var dbManager = DbManager(this)
